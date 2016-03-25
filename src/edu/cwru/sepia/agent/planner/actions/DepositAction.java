9b8f7eaa0661855d8a6fffbbaadb3c5f8f5250b7
pianonerd7 <pianonerd7@gmail.com>
2016-03-24 21:32:46 -0400

    implemented deposit apply

--- src/edu/cwru/sepia/agent/planner/actions/DepositAction.java
+++ src/edu/cwru/sepia/agent/planner/actions/DepositAction.java
@@ -2,6 +2,7 @@ package edu.cwru.sepia.agent.planner.actions;
 
 import edu.cwru.sepia.agent.planner.GameState;
 import edu.cwru.sepia.agent.planner.Peasant;
+import edu.cwru.sepia.environment.model.state.ResourceType;
 
 public class DepositAction implements StripsAction {
 
@@ -18,8 +19,25 @@ public class DepositAction implements StripsAction {
 
 	@Override
 	public GameState apply(GameState state) {
-		// TODO Auto-generated method stub
-		return null;
+
+		GameState newState = new GameState(this, state, state.getPeasant(), state.getForests(), state.getGoldMines(),
+				state.getTownHall(), state.getGoalWood(), state.getGoalGold(), state.getMyWood(), state.getMyGold(),
+				state.getPlayerNum(), state.getState());
+
+		ResourceType resourceType = newState.getPeasant().getHoldingObject();
+		newState.getPeasant().resetNextTo();
+
+		if (resourceType.equals("WOOD")) {
+			newState.setMyWood(newState.getMyWood() + newState.getPeasant().getResourceQuantity());
+		} else if (resourceType.equals("GOLD")) {
+			newState.setMyGold(newState.getMyGold() + newState.getPeasant().getResourceQuantity());
+		}
+
+		newState.getPeasant().setResourceQuantity(0);
+		newState.getPeasant().setHoldingObject(null);
+		newState.getPeasant().setIsEmpty(true);
+		newState.getPeasant().setNextToTownHall(true);
+		return newState;
 	}
 
 	@Override
